4a7768ac69a3ac168a76d538aa42709912c21d47
Pan_Janke <jan.krol.legowski@gmail.com>
2024-06-10 01:47:03 +0200

    wrapper component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import Button from "./components/Button.tsx";
 function App() {
-  return <h1>Let's get started!</h1>;
+  return (
+      <main>
+            <p>
+                <Button el={"button"}>A Button</Button>
+            </p>
+            <p>
+                <Button el={"anchor"} href = 'https://google.com'>A link</Button>
+            </p>
+      </main>
+  );
 }
 
 export default App;
